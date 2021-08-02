6abc8ca234572408b6aaf2f6495f42cbb1efc2d0
Your Name <you@example.com>
2021-08-02 14:45:39 +0300

    Commit

---
+++ JWTFreeCodeSpot_Original/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JWTFreeCodeSpot_Original.Models
+{
+    public class User
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Role { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
---
+++ JWTFreeCodeSpot_Original/Repository/IJWTAuthManager.cs
@@ -0,0 +1,13 @@
+﻿using Dapper;
+using JWTFreeCodeSpot_Original.Models;
+using System.Collections.Generic;
+
+namespace JWTFreeCodeSpot_Original.Repository
+{
+    public interface IJWTAuthManager
+    {
+        Response<string> GenerateJWT(User user);
+        Response<T> Execute_Command<T>(string query, DynamicParameters sp_params);
+        Response<List<T>> getUserList<T>();
+    }
+}
